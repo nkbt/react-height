725cf554075ec8ad040eee61647b42a27b184a0a
Nik Butenko <nik@butenko.me>
2015-10-30 21:49:11 +1100

    Self-destruct when height is measured (turned off by default)

--- src/ReactHeight.js
+++ src/ReactHeight.js
@@ -5,14 +5,33 @@ import {shouldComponentUpdate} from 'react-addons-pure-render-mixin';
 const ReactHeight = React.createClass({
   propTypes: {
     children: React.PropTypes.node.isRequired,
-    onHeightReady: React.PropTypes.func.isRequired
+    onHeightReady: React.PropTypes.func.isRequired,
+    hidden: React.PropTypes.bool
+  },
+
+
+  getDefaultProps() {
+    return {hidden: false};
+  },
+
+
+  componentWillMount() {
+    this.height = -1;
   },
 
 
   componentDidMount() {
     this.height = this.refs.wrapper.clientHeight;
-    if (this.height > 0) {
+    if (this.height > -1) {
+      this.forceUpdate();
       return this.props.onHeightReady(this.height);
+    }
+  },
+
+
+  componentWillReceiveProps({children}) {
+    if (children !== this.props.children) {
+      this.height = -1;
     }
   },
 
@@ -21,21 +40,26 @@ const ReactHeight = React.createClass({
 
 
   componentDidUpdate() {
-    if (this.refs.wrapper.clientHeight > 0 && this.refs.wrapper.clientHeight !== this.height) {
-      this.height = this.refs.wrapper.clientHeight;
-      return this.props.onHeightReady(this.height);
+    if (this.refs.wrapper) {
+      if (this.refs.wrapper.clientHeight !== this.height) {
+        this.height = this.refs.wrapper.clientHeight;
+        this.props.onHeightReady(this.height);
+      }
+      this.forceUpdate();
     }
   },
 
 
   render() {
-    const {onHeightReady, children, ...props} = this.props;
+    const {onHeightReady, hidden, children, ...props} = this.props;
+
+    if (this.height > -1) {
+      return hidden ? null : <div {...props}>{children}</div>;
+    }
 
     return (
       <div style={{height: 0, overflow: 'hidden'}}>
-        <div ref="wrapper" {...props}>
-          {children}
-        </div>
+        <div ref="wrapper" {...props}>{children}</div>
       </div>
     );
   }

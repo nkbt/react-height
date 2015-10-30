d54e46c25d383f169db26de2cb6c79fa4a44b0c9
Nik Butenko <nik@butenko.me>
2015-10-30 21:01:55 +1100

    Implement height reporter

--- src/ReactHeight.js
+++ src/ReactHeight.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import {shouldComponentUpdate} from 'react-addons-pure-render-mixin';
 
 
 const ReactHeight = React.createClass({
+  propTypes: {
+    children: React.PropTypes.node.isRequired,
+    onHeightReady: React.PropTypes.func.isRequired
+  },
+
+
+  componentDidMount() {
+    this.height = this.refs.wrapper.clientHeight;
+    if (this.height > 0) {
+      return this.props.onHeightReady(this.height);
+    }
+  },
+
+
+  shouldComponentUpdate,
+
+
+  componentDidUpdate() {
+    if (this.refs.wrapper.clientHeight > 0 && this.refs.wrapper.clientHeight !== this.height) {
+      this.height = this.refs.wrapper.clientHeight;
+      return this.props.onHeightReady(this.height);
+    }
+  },
+
+
   render() {
-    return <div>ReactHeight</div>;
+    const {onHeightReady, children, ...props} = this.props;
+
+    return (
+      <div style={{height: 0, overflow: 'hidden'}}>
+        <div ref="wrapper" {...props}>
+          {children}
+        </div>
+      </div>
+    );
   }
 });
 

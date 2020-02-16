70a8f98ebfe3b77555d899beba8cc894b92965ee
lanhhv84 <lanhhv84@gmail.com>
2020-02-16 11:00:47 +0700

    Replace unsafe lifecycle with hook

--- src/ReactHeight.js
+++ src/ReactHeight.js
@@ -1,107 +1,77 @@
-import React, {PureComponent} from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 
 
-const getElementHeightDefault = el => el.clientHeight;
+const getElementHeightDefault = el => (el ? el.clientHeight : 0);
 
+export const ReactHeight = ({
+  children,
+  hidden,
+  dirty,
+  getElementHeight,
+  onHeightReady,
+  ...props
+}) => {
+  const [dirtyState, setDirtyState] = useState(true);
+  const [shouldNotify, setShouldNotify] = useState(dirty);
+  const [height, setHeight] = useState(0);
+  const [wrapper, setWrapper] = useState(null);
 
-export class ReactHeight extends PureComponent {
-  static propTypes = {
-    children: PropTypes.node.isRequired,
-    onHeightReady: PropTypes.func.isRequired,
-    hidden: PropTypes.bool,
-    dirty: PropTypes.bool,
-    getElementHeight: PropTypes.func
-  };
+  useEffect(() => {
+    onHeightReady(height);
+  }, [height]);
 
-
-  static defaultProps = {
-    hidden: false,
-    dirty: true,
-    getElementHeight: getElementHeightDefault
-  };
-
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      dirty: props.dirty,
-      height: 0
-    };
-  }
-
-
-  componentDidMount() {
-    const {getElementHeight} = this.props;
-    const height = getElementHeight(this.wrapper);
-    const dirty = false;
-
-    this.setState({height, dirty}, () => {
-      const {onHeightReady} = this.props;
-      const {height: currentHeight} = this.state;
-      onHeightReady(currentHeight);
-    });
-  }
-
-
-  // eslint-disable-next-line react/no-deprecated
-  componentWillReceiveProps({children, dirty}) {
-    const {children: oldChildren} = this.props;
-    if (children !== oldChildren || dirty) {
-      this.setState({dirty: true});
+  useEffect(() => {
+    if (dirtyState) {
+      const elementHeight = getElementHeight(wrapper);
+      if (elementHeight !== height) {
+        setHeight(elementHeight);
+        setShouldNotify(false);
+      } else if (shouldNotify) {
+        onHeightReady(height);
+        setShouldNotify(false);
+      }
+      setDirtyState(false);
     }
+  });
+
+  useEffect(() => {
+    setDirtyState(true);
+  }, [children]);
+
+  useEffect(() => {
+    if (dirty) {
+      setDirtyState(true);
+      setShouldNotify(true);
+    }
+  }, [dirty]);
+
+  if (hidden && !dirtyState) {
+    setWrapper(null);
+    return null;
   }
 
-
-  componentDidUpdate() {
-    const {getElementHeight} = this.props;
-    const height = getElementHeight(this.wrapper);
-    const dirty = false;
-
-    const {height: currentSavedHeight} = this.state;
-
-    if (height === currentSavedHeight) {
-      // eslint-disable-next-line react/no-did-update-set-state
-      this.setState({dirty});
-    } else {
-      // eslint-disable-next-line react/no-did-update-set-state
-      this.setState({height, dirty}, () => {
-        const {onHeightReady} = this.props;
-        const {height: currentHeight} = this.state;
-        onHeightReady(currentHeight);
-      });
-    }
+  if (hidden) {
+    return (
+      <div style={{height: 0, overflow: 'hidden'}}>
+        <div ref={el => { setWrapper(el); }} {...props}>{children}</div>
+      </div>
+    );
   }
 
+  return <div ref={el => { setWrapper(el); }} {...props}>{children}</div>;
+};
 
-  setWrapperRef = el => {
-    this.wrapper = el;
-  };
+ReactHeight.propTypes = {
+  children: PropTypes.node.isRequired,
+  onHeightReady: PropTypes.func.isRequired,
+  hidden: PropTypes.bool,
+  dirty: PropTypes.bool,
+  getElementHeight: PropTypes.func
+};
 
-
-  render() {
-    const {
-      onHeightReady: _onHeightReady,
-      getElementHeight: _getElementHeight,
-      dirty: _dirty,
-      hidden,
-      children,
-      ...props
-    } = this.props;
-    const {dirty} = this.state;
-
-    if (hidden && !dirty) {
-      return null;
-    }
-
-    if (hidden) {
-      return (
-        <div style={{height: 0, overflow: 'hidden'}}>
-          <div ref={this.setWrapperRef} {...props}>{children}</div>
-        </div>
-      );
-    }
-
-    return <div ref={this.setWrapperRef} {...props}>{children}</div>;
-  }
-}
+ReactHeight.defaultProps = {
+  hidden: false,
+  dirty: true,
+  getElementHeight: getElementHeightDefault
+};
